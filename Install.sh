468855b108d60aa5b7bde55de584da93278fdb09
Zérock <golden0kamia@gmail.com>
2018-07-16 21:55:15 +0200

    add font

--- Install.sh
+++ Install.sh
@@ -1,2 +1,5 @@
 sudo apt-get update --yes
 sudo apt-get install python3 python3-tk python3-pil --yes
+
+sudo mkdir /usr/share/fonts/truetype/Poetsen\ One
+sudo mv Poetsen\ One /usr/share/fonts/truetype/Poetsen\ One/
