7610d7e469ce0f6bccc9f5dc2d7b40e981cb90d9
Bastien Piguet <golden0kamia@gmail.com>
2018-07-16 20:28:09 +0000

    modified:   Install.sh 	new file:   run.sh

--- Install.sh
+++ Install.sh
@@ -4,4 +4,4 @@ sudo apt-get install python3 python3-tk python3-pil --yes
 
 echo Installing Font
 sudo mkdir /usr/share/fonts/truetype/Poetsen\ One
-sudo mv asset/Poetsen\ One.ttf /usr/share/fonts/truetype/Poetsen\ One/
+sudo cp asset/Poetsen\ One.ttf /usr/share/fonts/truetype/Poetsen\ One/
