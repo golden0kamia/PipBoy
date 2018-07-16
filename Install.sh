403c0531871144a469ff9b526f476e2fdcaa5552
Zérock <golden0kamia@gmail.com>
2018-07-16 22:16:01 +0200

    modified install

--- Install.sh
+++ Install.sh
@@ -1,7 +1,7 @@
 sudo apt-get update --yes
-print('Installing dependencies')
+print("Installing dependencies")
 sudo apt-get install python3 python3-tk python3-pil --yes
 
-print('Installing Font')
+print("Installing Font")
 sudo mkdir /usr/share/fonts/truetype/Poetsen\ One
 sudo mv Poetsen\ One /usr/share/fonts/truetype/Poetsen\ One/
