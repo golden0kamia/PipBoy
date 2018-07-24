0bc7bd16db31572ca454a2b148bc3cb48615686b
Zérock <golden0kamia@gmail.com>
2018-07-24 19:04:53 +0200

    adapt screen

--- main.py
+++ main.py
@@ -140,7 +140,7 @@ def main():
     frequence = StringVar()
     frequence.set('87.5Mhz')
     
-    freq = Label(Setting, font='Poetsen\ one -80', textvariable=frequence, bg='black', fg=green, bd=10, relief='sunken', width=10, justify='center')
+    freq = Label(Setting, font='Poetsen\ one -80', textvariable=frequence, bg='black', fg=green, bd=10, relief='sunken', width=9, justify='center')
     freq.grid(row=1, column=0, columnspan=5, sticky='e', ipady=30, padx=20, pady=5)
     
     imgprev = PhotoImage(file='asset/prev.png')
