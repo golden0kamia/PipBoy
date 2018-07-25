827316ede215c46b2d22a7da47d2cf54b27400e7
Bastien Piguet <golden0kamia@gmail.com>
2018-07-25 07:34:13 +0200

    add dependencies

--- Install.sh
+++ Install.sh
@@ -1,6 +1,6 @@
 #sudo apt-get update
 echo Installing dependencies
-sudo apt-get install python3 python3-tk python3-pil
+sudo apt-get install python3 python3-tk python3-pil libsdl-image1.2-dev libdevil-dev libglc-dev freeglut3-dev libxmu-dev libfribidi-dev libglc-dev freeglut3-dev libgl1-mesa-dev libqt4-dev libgps-dev
 echo Installing Font
 sudo mkdir /usr/share/fonts/truetype/Poetsen\ One/
 sudo cp asset/Poetsen\ One.ttf /usr/share/fonts/truetype/Poetsen\ One/
