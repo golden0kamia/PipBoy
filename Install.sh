b97b43089fcfa06fe47854224d9391ffd5548def
Bastien Piguet <golden0kamia@gmail.com>
2022-08-17 22:16:38 +0200

    try embed map

--- Install.sh
+++ Install.sh
@@ -1,6 +1,7 @@
-#sudo apt-get update
-echo Installing dependencies
-sudo apt-get -y install python3 python3-tk python3-pil libsdl-image1.2-dev libdevil-dev libglc-dev freeglut3-dev libxmu-dev libfribidi-dev libglc-dev freeglut3-dev libgl1-mesa-dev libqt4-dev libgps-dev -y
-echo Installing Font
-sudo mkdir /usr/share/fonts/truetype/Poetsen\ One/
-sudo cp asset/Poetsen\ One.ttf /usr/share/fonts/truetype/Poetsen\ One/
+sudo apt-get update
+sudo apt install python3-pyqt5 --yes
+#echo Installing dependencies
+#sudo apt-get -y install python3 python3-tk python3-pil libsdl-image1.2-dev libdevil-dev libglc-dev freeglut3-dev libxmu-dev libfribidi-dev libglc-dev freeglut3-dev libgl1-mesa-dev libqt4-dev libgps-dev -y
+#echo Installing Font
+#sudo mkdir /usr/share/fonts/truetype/Poetsen\ One/
+#sudo cp asset/Poetsen\ One.ttf /usr/share/fonts/truetype/Poetsen\ One/
--- main.py
+++ main.py
@@ -1,45 +1,71 @@
 import sys
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import QProcess
+from PyQt5.QtGui import QWindow
 
 def say_hello():
     print("Hello wolrd!")
 
 app = QApplication(sys.argv)
 
+win = QWidget()
+winID = int(win.winId())
+
+sub_win = QWindow.fromWinId(winID)
+container = QWidget.createWindowContainer(sub_win)
+sub_win_id = int(container.winId())
+
+process = QProcess(container)
+process.start("navit")
+
 with open("style.qss", 'r') as f:
     _style = f.read()
     app.setStyleSheet(_style)
 
+#Main window widget
 root = QWidget()
 root.resize(800, 480)
 root.setWindowTitle("PipBoyQt")
 root.show()
 
-'''hour = QLabel(root)
+#Main vertical box
+mainVbox = QVBoxLayout(root)
+
+#Top bar for time et settings button
+topBar = QHBoxLayout()
+mainVbox.addLayout(topBar)
+
+#Date and hour display
+hour = QLabel()
 hour.setText("17.08.2022 17:08")
-hour.show()'''
+topBar.addWidget(hour)
 
-'''button = QPushButton(root)
-button.setText("Click me!")
-button.clicked.connect(say_hello)
-button.show()'''
+#Space between the date andthe settings button
+topBarSpacer = QSpacerItem(10, 0)
+topBar.addSpacerItem(topBarSpacer)
 
-'''tabs = QTabBar(root)
-tabs.addTab("Radio")
-tabs.addTab("Map")
-tabs.addTab("Calendar")
-tabs.show()'''
+#Settings button
+settingsButton = QPushButton()
+settingsButton.setText("Click me!")
+settingsButton.clicked.connect(say_hello)
+topBar.addWidget(settingsButton)
 
+#Radio tab
 radioW = QWidget()
 radio1 = QPushButton("Radio 1", radioW)
 radio2 = QPushButton("Radio 2", radioW)
 
+#Navit maps tab
+
+
+#Calendar tab
 cal = QCalendarWidget()
 
+#Tabs wigets
 tabs = QTabWidget(root)
 tabs.addTab(radioW, "Radio")
-#tabs.addTab("Map")
+tabs.addTab(container, "Map")
 tabs.addTab(cal, "Calendar")
-tabs.show()
+mainVbox.addWidget(tabs)
 
 sys.exit(app.exec_())
